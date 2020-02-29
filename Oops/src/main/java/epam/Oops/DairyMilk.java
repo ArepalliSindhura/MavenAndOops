f182d148d36149260a2356411f16f7265984dbd3
Sindhura <Sindhura@192.168.0.5>
2020-02-29 22:09:29 +0530

    Added 2 classes of chocolates

---
+++ Oops/src/main/java/epam/Oops/DairyMilk.java
@@ -0,0 +1,43 @@
+package epam.Oops;
+
+public class DairyMilk extends PlainChocolate {
+	float weight;
+	float price;
+	int quantity;
+	public float getWeight() {
+		return weight;
+	}
+	public void setWeight(float weight) {
+		this.weight = weight;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public DairyMilk(float weight, float price, int quantity, float weight2, float price2, int quantity2) {
+		super(weight, price, quantity);
+		weight = weight2;
+		price = price2;
+		quantity = quantity2;
+	}
+	public DairyMilk() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public DairyMilk(float weight, float price, int quantity) {
+		super(weight, price, quantity);
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "DairyMilk [weight=" + weight + ", price=" + price + ", quantity=" + quantity + "]";
+	}
+}
